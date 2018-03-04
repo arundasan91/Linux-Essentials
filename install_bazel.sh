f67592436ef910973886f35e2afec32aa8817dbd
Arun Das <arun.das@my.utsa.edu>
2018-03-04 14:44:30 -0600

    Update install_bazel.sh

--- install_bazel.sh
+++ install_bazel.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 echo "deb [arch=amd64] http://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
 curl https://storage.googleapis.com/bazel-apt/doc/apt-key.pub.gpg | sudo apt-key add -
-sudo apt-get update && sudo apt-get install bazel
+sudo apt-get update && sudo apt-get -y install bazel
 sudo apt-get upgrade bazel
