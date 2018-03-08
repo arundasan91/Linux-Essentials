1536100df43324ffd2ac6ccf958cbb17f540324d
Arun Das <arun.das@my.utsa.edu>
2018-03-08 11:06:44 -0600

    Update install_mysql.sh

--- install_mysql.sh
+++ install_mysql.sh
@@ -1,12 +1,13 @@
-sudo apt-get install python-dev python3-dev
-sudo apt-get install mysql-client-core-5.7
-sudo apt-get install mysql-server
-sudo apt-get install libmysqlclient-dev
+#!/bin/bash
+sudo apt-get -y install python-dev python3-dev
+sudo apt-get -y install mysql-client-core-5.7
+sudo apt-get -y install mysql-server
+sudo apt-get -y install libmysqlclient-dev
 pip3 install --user configparser
 pip3 install --user mysqlclient
 
 # Alternate Method
-wget https://dev.mysql.com/get/Downloads/Connector-Python/mysql-connector-python-cext-py3_2.1.7-1ubuntu16.04_amd64.deb
-sudo dpkg -i mysql-connector-python-cext-py3_2.1.7-1ubuntu16.04_amd64.deb
-sudo apt-get update
-# sudo apt-get install 
+# wget https://dev.mysql.com/get/Downloads/Connector-Python/mysql-connector-python-cext-py3_2.1.7-1ubuntu16.04_amd64.deb
+# sudo dpkg -i mysql-connector-python-cext-py3_2.1.7-1ubuntu16.04_amd64.deb
+# sudo apt-get update
+# sudo apt-get install <>
