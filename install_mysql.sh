573736114db045c33532af0899b92027e98cd93c
Arun Das <arun.das@my.utsa.edu>
2018-03-08 10:55:17 -0600

    Create install_mysql.sh

---
+++ install_mysql.sh
@@ -0,0 +1,10 @@
+sudo apt-get install mysql-client-core-5.7
+sudo apt-get install mysql-server
+pip3 install --user configparser
+pip3 install --user mysqlclient
+
+# Alternate Method
+wget https://dev.mysql.com/get/Downloads/Connector-Python/mysql-connector-python-cext-py3_2.1.7-1ubuntu16.04_amd64.deb
+sudo dpkg -i mysql-connector-python-cext-py3_2.1.7-1ubuntu16.04_amd64.deb
+sudo apt-get update
+# sudo apt-get install 
