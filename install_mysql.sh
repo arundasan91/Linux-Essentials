a46758dfe5de5248e2405db85e64f49fe082f7ef
Arun Das <arun.das@my.utsa.edu>
2018-03-08 11:03:37 -0600

    Update install_mysql.sh

--- install_mysql.sh
+++ install_mysql.sh
@@ -1,5 +1,7 @@
+sudo apt-get install python-dev python3-dev
 sudo apt-get install mysql-client-core-5.7
 sudo apt-get install mysql-server
+sudo apt-get install libmysqlclient-dev
 pip3 install --user configparser
 pip3 install --user mysqlclient
 
