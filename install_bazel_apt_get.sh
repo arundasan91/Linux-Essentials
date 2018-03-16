7f301eefdf96d74e31e127e2b287e741bfc78308
arundasan91 <arun.das@my.utsa.edu>
2018-03-16 21:39:23 +0000

    fixed jdk9 install bug in bazel script

--- install_bazel_apt_get.sh
+++ install_bazel_apt_get.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 echo "deb [arch=amd64] http://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
 curl https://storage.googleapis.com/bazel-apt/doc/apt-key.pub.gpg | sudo apt-key add -
+sudo apt-get -o Dpkg::Options::="--force-overwrite" install openjdk-9-jdk
 sudo apt-get -y update && sudo apt-get -y install bazel
 sudo apt-get -y upgrade bazel
