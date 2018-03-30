e1759b3a39d1c394adc1cab661c3d7b1b45facea
arundasan91 <arun.das@my.utsa.edu>
2018-03-30 19:37:07 +0000

    remove ipynbcheckpoint

---
+++ install_protoc.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+PROTOC_ZIP=protoc-3.3.0-linux-x86_64.zip
+curl -OL https://github.com/google/protobuf/releases/download/v3.3.0/$PROTOC_ZIP
+sudo unzip -o $PROTOC_ZIP -d /usr/local bin/protoc
+rm -f $PROTOC_ZIP
