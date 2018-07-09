68d3ae2ef30163fbca0628a26429cc8172582a56
Arun Das <14352842+arundasan91@users.noreply.github.com>
2018-07-08 21:55:22 -0500

    Update install_nvidia_docker.sh
    extra user permission fix

--- install_nvidia_docker.sh
+++ install_nvidia_docker.sh
@@ -19,6 +19,7 @@ sudo add-apt-repository \
 sudo apt-get -y update
 sudo apt-get -y install docker-ce
 sudo usermod -a -G docker $USER
+sudo chmod 666 /var/run/docker.sock
 # INSTALL NVIDIA-DOCKER
 wget -P /tmp https://github.com/NVIDIA/nvidia-docker/releases/download/v1.0.1/nvidia-docker_1.0.1-1_amd64.deb
 sudo dpkg -i /tmp/nvidia-docker*.deb && rm /tmp/nvidia-docker*.deb
