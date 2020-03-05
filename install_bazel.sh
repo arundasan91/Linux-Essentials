fec70400ed20a3f5b8c00cefdee71ac62977db28
Arun Das <14352842+arundasan91@users.noreply.github.com>
2020-03-05 14:54:17 -0600

    Update Bazel version to 1.2.1

--- install_bazel.sh
+++ install_bazel.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-curl https://github.com/bazelbuild/bazel/releases/download/0.6.1/bazel-0.6.1-installer-linux-x86_64.sh | sudo bash
+curl https://github.com/bazelbuild/bazel/releases/download/1.2.1/bazel-1.2.1-installer-linux-x86_64.sh | sudo bash
