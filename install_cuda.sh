a51acdfdd7e5f905571633ad2bb2ff4960650a07
Arun Das <14352842+arundasan91@users.noreply.github.com>
2021-01-27 11:22:19 -0600

    Update install_cuda.sh

--- install_cuda.sh
+++ install_cuda.sh
@@ -3,4 +3,10 @@ sudo mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
 sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
 sudo add-apt-repository "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/ /"
 sudo apt-get update
+sudo apt-get -y upgrade
 sudo apt-get -y install cuda-10-2
+sudo apt-get -y install vim htop dstat
+cat <<'EOT' >> ~/.bashrc
+export PATH=/usr/local/cuda-10.2/bin:$PATH
+export LD_LIBRARY_PATH=/usr/local/cuda-10.2/lib64:$LD_LIBRARY_PATH
+EOT
